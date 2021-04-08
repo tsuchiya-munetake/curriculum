375d53f68b8f2cffeacb47677febc510519f7f7b
tsuchiya-munetake <79914906+tsuchiya-munetake@users.noreply.github.com>
2021-04-08 20:59:54 +0900

    Create Task1_1.java
    Task1_1.class

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,66 @@
+/**
+ * Task1_1: 課題内容
+ *
+ * 本課題では、データ型・文字列の使い方を学んでいきましょう。
+ * 問①から問⑧まであります。
+ * 指定された型・値・変数名を守って記述してください。
+ * 問③,⑦については、コメントを入れてください。
+ *
+ */
+public class Task1_1 {
+
+    public static void main(String[] args) {
+
+        // ①int型の hogeInt という名前の変数を宣言し、値「50」で初期化
+        int hougeInt=50
+
+        // ②Stringクラスの hogeString という名前の変数を宣言、値「テスト」で初期化
+        String hogeString=テスト
+
+        // ③下記の処理について、何をしているのかコメントを記入してください
+        // /**
+ * Task1_1: 課題内容
+ *
+ * 本課題では、データ型・文字列の使い方を学んでいきましょう。
+ * 問①から問⑧まであります。
+ * 指定された型・値・変数名を守って記述してください。
+ * 問③,⑦については、コメントを入れてください。
+ *
+ */
+public class Task1_1 {
+
+    public static void main(String[] args) {
+
+        // ①int型の hogeInt という名前の変数を宣言し、値「50」で初期化
+        hougeInt=50
+
+        // ②Stringクラスの hogeString という名前の変数を宣言、値「テスト」で初期化
+        hougeInt=テスト
+
+        // ③下記の処理について、何をしているのかコメントを記入してください
+        // double hogeDouble - Task1_1.main(...).Task1_1.main(String[])
+        double hogeDouble = 0.2;
+
+        // ④変数 hogeInt を値「100」で上書き
+        hougeInt=100
+        // ⑤変数 hogeString を値「合格」で上書き
+        hougeInt=合格
+
+        // ⑥変数 hogeInt の値を表示
+        hougeInt=100
+
+        // ⑦下記の処理について、何をしているのかコメントを記入してください
+        // void java.io.PrintStream.println(String x)
+Prints a String and then terminate the line. This method behaves as though it invokes print(String) and then println().
+
+Parameters:
+x The String to be printed
+        System.out.println(hogeString);
+
+        // ⑧変数 hogeDouble の値を表示
+        ouble hogeDoubl - Task1_1.main(...).Task1_1.print()
+Syntax error on token(s), misplaced construct(s)Java(1610612972)
+
+
+    }
+}
