49dbc93c3d77d92f2c6fbc4966d7a93f6b94cf33
tsuchiya-munetake <79914906+tsuchiya-munetake@users.noreply.github.com>
2021-05-06 15:57:31 +0900

    課題提出テスト
    Task1_3.class

--- JavaStudy/1-3/Task1_3.java
+++ JavaStudy/1-3/Task1_3.java
@@ -14,17 +14,22 @@ public class Task1_3 {
     public static void main(String[] args) {
 
         // ① 5 と 10 を加算した値を代入する、変数 plus を作成しなさい。
-        plus=5+10
+        int i=0
+        i=5+10
+        System.out.println(plus);
 
         // ② 20 から 7 を減算した値を代入する、変数 minus を作成しなさい。
-        minus=20-7
-
+        String i=0
+        i=20-7
+        System.out.println(minus);
         // ③下記の処理について、何をしているのかコメントを記入してください。
-        // 10と2を乗算した結果を変数multiplyに代入
+        // 10と2を乗算した値に代入する、変数multiplyを作成する。
         int multiply = 10 * 2;
 
         // ④ 20 を 6 で割った余りを代入する、変数 remi を作成しなさい。
-        remi=20/6
+        int a=0
+        a=20÷6
+        System.out.println(remi);
 
         // ⑤下記の処理について、何をしているのかコメントを記入してください
         // 有楽町という町には有楽町線という路線がある。
@@ -32,14 +37,16 @@ public class Task1_3 {
         String line = "線";
 
         // ⑥ ⑤で作成した変数を連結させた値を代入する変数 train を作成しなさい
-        train=有楽町％線
+        String a=0
+        a=(有楽町＝線);
+        System.out.println(train);
 
         // ⑦ 変数plus, minus, multiply, remi, train をそれぞれ出力しなさい。
-        plus=+
-        minus=-
-        multiply=*
-        remi=/
-        train=％
+        plus=15
+        minus=13
+        multiply=20
+        remi=2
+        train=有楽町線
 
     }
 }
