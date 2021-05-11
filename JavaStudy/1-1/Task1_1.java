dbd72caaf83c5e74c78d14d322f587f92fab1bc4
tsuchiya-munetake <79914906+tsuchiya-munetake@users.noreply.github.com>
2021-05-11 18:24:01 +0900

    課題提出テスト
    Task1_1.class

--- JavaStudy/1-1/Task1_1.java
+++ JavaStudy/1-1/Task1_1.java
@@ -32,33 +32,33 @@ public class Task1_1 {
     public static void main(String[] args) {
 
         // ①int型の hogeInt という名前の変数を宣言し、値「50」で初期化
-        int hougeInt=50
+        int hougeInt=50;
         System.out.print=(hogeInt);
 
         // ②Stringクラスの hogeString という名前の変数を宣言、値「テスト」で初期化
-       String hougeString=テスト
+       String hougeString="テスト";
        System.out.print=(hougeString);
 
         // ③下記の処理について、何をしているのかコメントを記入してください
-        0.2から表示できる；
+        //double1のhogeDoubleという名前の変数を宣言、値「0.2」で初期化
         double hogeDouble = 0.2;
 
         // ④変数 hogeInt を値「100」で上書き
-        int hogeInt=100
-        System.out.print=(hogeInt);
+        hogeInt=100;
+        System.out.println(hogeInt);
         // ⑤変数 hogeString を値「合格」で上書き
-        String hougeString=合格
-        System.out.print=(hougeString);
+        hougeString="合格";
+        System.out.println(hogeString);
 
         // ⑥変数 hogeInt の値を表示
-        System.out.ptintln=("int型=hogeInt="+100);
+        System.out.ptintln=(100);
 
         // ⑦下記の処理について、何をしているのかコメントを記入してください
-        １００点で合格になる。
+        //hogeStringを「合格」上書き
         System.out.println(hogeString);
 
         // ⑧変数 hogeDouble の値を表示
-        System.out.println("Stringクラス=hogeDouBle"+合格);
+        System.out.println(0.2);
         
 
 
