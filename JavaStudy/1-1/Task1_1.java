4f9d82a6890dd0c003617d9d5b866e4a6fffb062
tsuchiya-munetake <79914906+tsuchiya-munetake@users.noreply.github.com>
2021-05-11 18:37:36 +0900

    課題提出テスト
    Task1_1.class

--- JavaStudy/1-1/Task1_1.java
+++ JavaStudy/1-1/Task1_1.java
@@ -1,43 +1,15 @@
-/**
- * Task1_1: 課題内容
- *
- * 本課題では、データ型・文字列の使い方を学んでいきましょう。
- * 問①から問⑧まであります。
- * 指定された型・値・変数名を守って記述してください。
- * 問③,⑦については、コメントを入れてください。
- *
- */
+
 public class Task1_1 {
 
     public static void main(String[] args) {
 
         // ①int型の hogeInt という名前の変数を宣言し、値「50」で初期化
-        int hougeInt=50
+        int hogeInt=50;
+        System.out.print(hogeInt);
 
         // ②Stringクラスの hogeString という名前の変数を宣言、値「テスト」で初期化
-        String hogeString=テスト
-
-        // ③下記の処理について、何をしているのかコメントを記入してください
-        // /**
- * Task1_1: 課題内容
- *
- * 本課題では、データ型・文字列の使い方を学んでいきましょう。
- * 問①から問⑧まであります。
- * 指定された型・値・変数名を守って記述してください。
- * 問③,⑦については、コメントを入れてください。
- *
- */
-public class Task1_1 {
-
-    public static void main(String[] args) {
-
-        // ①int型の hogeInt という名前の変数を宣言し、値「50」で初期化
-        int hougeInt=50;
-        System.out.print=(hogeInt);
-
-        // ②Stringクラスの hogeString という名前の変数を宣言、値「テスト」で初期化
-       String hougeString="テスト";
-       System.out.print=(hougeString);
+       String hogeString="テスト";
+       System.out.print(hogeString);
 
         // ③下記の処理について、何をしているのかコメントを記入してください
         //double1のhogeDoubleという名前の変数を宣言、値「0.2」で初期化
@@ -47,14 +19,14 @@ public class Task1_1 {
         hogeInt=100;
         System.out.println(hogeInt);
         // ⑤変数 hogeString を値「合格」で上書き
-        hougeString="合格";
+        hogeString="合格";
         System.out.println(hogeString);
 
         // ⑥変数 hogeInt の値を表示
-        System.out.ptintln=(100);
+        System.out.println(100);
 
         // ⑦下記の処理について、何をしているのかコメントを記入してください
-        //hogeStringを「合格」上書き
+        //hogeStringを「合格」で上書き
         System.out.println(hogeString);
 
         // ⑧変数 hogeDouble の値を表示
