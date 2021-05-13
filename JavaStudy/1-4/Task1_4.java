292fc070df18326a33214ed96ef1c607c35b5f5d
tsuchiya-munetake <79914906+tsuchiya-munetake@users.noreply.github.com>
2021-05-13 13:21:01 +0900

    課題提出テスト
    Task1_4.class

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -39,23 +39,23 @@ public class Task1_4 {
 
         // ② 「name」の値が「USER_RNAME」と等しく、「 pass 」の値が「USER_PASSWORD」と等しい場合。
         //      定数を使用して「 ログイン成功です 」と出力してください。
-        if((name == USER_NAME) && (pass == USER_PASSWORD)){
-          System.out.println("ログイン成功です");
+        if((nameequalsUSER_NAME) && (passequalsUSER_PASSWORD)){
+          System.out.println(" private static final String CONST_MSG_SUCCESS");
           }
         // ③ 「USER_RNAME」の値のみ等しい場合。
        //     定数を使用して「 パスワードに誤りがあります。 」 と出力してください。
-        else if((name==USER_NAME) && (pass!=USER_PASSWORD)){
-        System.out.println("パスワードに誤りがあります。");
+        else if((nameequalsUSER_NAME) && (pass!=USER_PASSWORD)){
+        System.out.println("private static final String CONST_MSG_ERROR_PASS");
         }
         // ④ 「USER_PASSWORD」の値のみ等しい場合。
        //定数を使用して「 名前に誤りがあります。 」と出力してください。
-       else if ((name!=USER_NAME) && (pass==USER_PASSWORD)){
-        System.out.println("名前に誤りがあります。");
+       else if ((name!=USER_NAME) && (passequalsUSER_PASSWORD)){
+        System.out.println("private static final String CONST_MSG_ERROR_NAME");
         }
         // ⑤ 「USER_RNAME」も「USER_PASSWORD」の値も間違っていた場合。 
        //定数を使用して「 入力情報に誤りがあります。 」と出力してください
        else {
-        System.out.println("入力情報に誤りがあります。");
+        System.out.println("private static final String CONST_MSG_ERROR_INPUT");
       }
     }
   }
